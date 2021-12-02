2d7bd67d1192b89a9dc2136ae17164370d0bd6e4
Gary Kaufman <gary.kaufman@ibm.com>
2021-12-02 16:03:56 -0600

    module1 2nd commit

--- stats/data.py
+++ stats/data.py
@@ -17,8 +17,16 @@ for game_file in game_files:
 
 games = pd.concat(game_frames)
 
-games.loc[multi5, []= ""]
+games.loc[games['multi5'] == '??', ['multi5']] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
-
 identifiers = identifiers.fillna(method='ffill')
+identifiers.columns = ['game_id', 'year']
+
+games = pd.concat([games, identifiers], axis=1, sort=False)
+
+games = games.fillna(' ')  # Fill the empty values with NaN
+
+games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+
+print(games.head())
